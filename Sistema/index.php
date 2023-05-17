c9d6206a63c113fd2ad0beb8548f1d932159940c
Ruan Pereira <91697818+jhonRPdoe@users.noreply.github.com>
2023-05-17 15:49:43 -0300

    Projeto concluido

--- Sistema/index.php
+++ Sistema/index.php
@@ -30,7 +30,7 @@
                                 <a href="<?= $BASE_URL ?>/edit.php?id=<?= $contact["id"] ?>"><i class="far fa-edit edit-icon"></i></a>
                                 <form class="delete-form" action="<?= $BASE_URL ?>/config/process.php" method="POST">
                                     <input type="hidden" name="type" value="delete">
-                                    <input type="hidden" name="type" value="<?= $contact["id"] ?>">
+                                    <input type="hidden" name="id" value="<?= $contact["id"] ?>">
                                     <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
                                 </form>
                             </td>
